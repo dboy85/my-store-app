4b3d0f288e1b17160d41b3b4026cb299ee403740
Didintle Tleane <tleaned@gmail.com>
2026-05-06 17:54:29 +0200

    asdf

--- src/App.js
+++ src/App.js
@@ -18,6 +18,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <section>
+        <h2>Welcome to React</h2>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </section>
     </div>
   );
 }
